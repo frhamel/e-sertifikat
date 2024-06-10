e7a78084157f6f21e71a9ad71ac807546ec6b766
Ardila Novita Sari <crwiszvita@gmail.com>
2024-06-10 17:00:55 +0700

    revisi sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -39,6 +39,34 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
+                @if (auth()->user()->role_id == 2)
+                <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+       with font-awesome or any other icon font library -->
+                <li class="nav-item menu-open">
+                    <a href="{{route ('dashboard_user') }}" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                            <i class="right badge badge-danger"></i>
+                        </p>
+                    </a>
+
+                    <li class="nav-item menu-open">
+                    <a href="{{route ('tbl_event.index') }}" class="nav-link">
+                        <i class="fas fa-calendar-alt"></i>
+                        <p>
+                            Data Event 
+                            <i class="right badge badge-danger"></i>
+                        </p>
+                    </a>
+                @else
+                <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
        with font-awesome or any other icon font library -->
                 <li class="nav-item">
@@ -97,6 +125,70 @@
                         </p>
                     </a>
                 </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+
+                @endif
+                <!-- Add icons to the links using the .nav-icon class
+       with font-awesome or any other icon font library -->
+                <!-- <li class="nav-item">
+                    <a href="{{ route('dashboard') }}" class="nav-link">
+                    <i class="fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                            <i class="right badge badge-danger"></i>
+                        </p>
+                    </a> -->
+                   <!-- <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('home') }}" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Sub menu 1</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('home') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Sub menu 2</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('home') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Sub menu 3</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>-->
+                <!-- <li class="nav-item">
+                   <a href="{{ route('template_design.index') }}" class="nav-link">
+                   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+                   <i class="fa-solid fa-palette"></i>
+                        <p>
+                             Template Design
+                            {{-- <span class="right badge badge-danger">New</span> --}}
+                        </p>
+        </a>
+        <li class="nav-item">
+                   <a href="{{ route('data_users.index') }}" class="nav-link">
+                   <i class="fa fa-user-circle"></i> 
+                        <p>
+                             Data Users
+                            {{-- <span class="right badge badge-danger">New</span> --}}
+                        </p>
+                    </a>
+        </li>
+                    <li class="nav-item">
+                   <a href="{{ route('tbl_peserta.index') }}" class="nav-link">
+                   <i class="fa-solid fa-user-plus"></i>
+                        <p>
+                             Peserta
+                            {{-- <span class="right badge badge-danger">New</span> --}}
+                        </p>
+                    </a>
+                </li> -->
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
